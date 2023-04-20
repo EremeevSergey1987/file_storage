0ae133f773f0712de8d2ae39f2f06ef84166be62
Sergey Eremeev <eremeev87@bk.ru>
2023-04-20 23:25:23 +0800

    Initial commit

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
-$urlList = [ '/funds/' => [ 'GET' => 'Funds::list()',
-'POST' => 'Funds::add()'
-]
+$urlList = ['/funds/' =>
+    ['GET' => Funds::list(),
+     'POST' => Funds::add(),
+    ],
 ];
+
+print_r($urlList);
